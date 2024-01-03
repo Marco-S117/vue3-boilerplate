5f7a176227b85d930a665e71c3ee006aafe24bd7
Marco Polino <55142606+Marco-S117@users.noreply.github.com>
2024-01-03 13:18:56 +0000

    chore: update config

--- config/eslint/index.js
+++ config/eslint/index.js
@@ -1,15 +1,18 @@
-// eslint.config.js
 const antfu = require('@antfu/eslint-config').default
 
 module.exports = antfu({
+  formatters: {
+    css: true,
+    markdown: true,
+  },
   stylistic: {
     indent: 2,
     quotes: 'single',
   },
   typescript: true,
   vue: true,
-  formatters: {
-    css: true,
-    markdown: true,
+  rules: {
+    'antfu/top-level-function': 'off',
+    'no-console': 'off',
   },
 })
