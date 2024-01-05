4a3260231b21f28f769483a63b7c77c1552b35ef
Marco Polino <55142606+Marco-S117@users.noreply.github.com>
2024-01-05 16:06:17 +0000

    feat: init AppImage component

---
+++ src/components/atoms/AppImage.vue
@@ -0,0 +1,31 @@
+<script lang='ts' setup>
+import { ref } from 'vue'
+
+const props = defineProps<{
+  alt?: string
+  draggable?: boolean
+  title?: string
+  src: string
+}>()
+
+const ready = ref(false)
+
+const onLoad = () => { ready.value = true }
+</script>
+
+<template>
+  <div class="app-image">
+    <img
+      v-bind="props"
+      :class="{ 'opacity-0': !ready }"
+      class="transition-opacity-500"
+      @load="onLoad"
+    >
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.app-image {
+  @apply inline-block;
+}
+</style>
