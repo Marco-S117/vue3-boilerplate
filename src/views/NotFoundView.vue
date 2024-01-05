516fc5c175189eb4820be1bbb6ed1f9615261356
Marco Polino <55142606+Marco-S117@users.noreply.github.com>
2024-01-05 16:25:11 +0000

    chore: update 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,25 +1,31 @@
 <script lang="ts" setup>
 import { useSeoMeta } from '@unhead/vue'
+import { useFetch } from '@vueuse/core'
+
+const { data } = useFetch('https://v2.jokeapi.dev/joke/Any?format=txt&safe-mode')
 
 useSeoMeta({
   title: '404',
-  description: 'My 404 page',
+  description: () => data.value || 'My 404 page description',
   // keywords: '',
-  ogDescription: 'Still 404 my 404 page',
+  ogDescription: () => data.value || 'description',
   ogUrl: window.location.href,
   ogTitle: '404',
   // ogImage: '@CHANGE_ME',
   // twitterDescription: '@CHANGE_ME',
   // twitterImage: '@CHANGE_ME',
-  twitterTitle: 'My 404 page'
+  // twitterTitle: '@CHANGE_ME',
   // twitterCard: '@CHANGE_ME'
 })
 </script>
 
 <template>
-  <div>
-    <h1 class="text-h1">
+  <div v-if="data" class="flex flex-col items-center justify-center gap-2">
+    <h1 class="text-h1 text-primary">
       404
     </h1>
+    <p class="font-italic">
+      {{ data }}
+    </p>
   </div>
 </template>
